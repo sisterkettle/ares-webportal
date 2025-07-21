668c9432dd3e5dccce3e5acbc8a2adaf03eac2ae
girlcalledblu <45247880+girlcalledblu@users.noreply.github.com>
2025-07-20 17:38:45 -0700

    Update account.js

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -4,83 +4,6 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default Controller.extend(AuthenticatedController, {
-<<<<<<< HEAD
-    newPassword: '',
-    currentPassword: '',
-    confirmPassword: '',
-    confirmPasswordHandle: '',
-    confirmPasswordSettings: '',
-    handleName: '',
-    linkCode: '',
-    session: service(),
-    flashMessages: service(),
-    gameApi: service(),
-
-    resetOnExit: function() {
-      this.set('currentPassword', '');
-      this.set('newPassword', '');
-      this.set('confirmPassword', '');
-      this.set('confirmPasswordHandle', '');
-      this.set('confirmPasswordSettings', '');
-      this.set('linkCode', '');
-      this.set('handleName', '');
-    },
-    actions: {
-
-        changePassword() {
-
-           this.gameApi.requestOne('changePassword',
-               { name: this.get('model.name'), current_password: this.currentPassword, confirm_password: this.confirmPassword, new_password: this.newPassword}, null)
-            .then((response) => {
-                if (response.error) {
-                    return;
-                }
-                this.resetOnExit();
-                this.flashMessages.success("Your password has been changed.");
-            });
-        },
-
-        changeSettings() {
-
-           this.gameApi.requestOne('updateAccountInfo',
-               { email: this.get('model.email'),
-                 name: this.get('model.name'),
-                 confirm_password: this.confirmPasswordSettings,
-                 timezone: this.get('model.timezone'),
-                 unified_play_screen: this.get('model.unified_play_screen'),
-//add in channel_handles option
-                 channel_handles: this.get('model.channel_handles')
-               }, null)
-            .then((response) => {
-                if (response.error) {
-                    return;
-                }
-                this.resetOnExit();
-                this.flashMessages.success("Your account settings have been changed.");
-            });
-        },
-
-        linkHandle() {
-
-           this.gameApi.requestOne('linkHandle',
-               { handle_name: this.handleName, link_code: this.linkCode, confirm_password: this.confirmPasswordHandle }, null)
-            .then((response) => {
-                if (response.error) {
-                    return;
-                }
-                this.set('model.handle', response.handle);
-                this.resetOnExit();
-                this.flashMessages.success("You have linked this character to your Ares player handle.");
-            });
-        },
-
-        timezoneChanged(val) {
-          this.set('model.timezone', val);
-        }
-
-
-    }
-=======
   newPassword: '',
   currentPassword: '',
   confirmPassword: '',
@@ -154,8 +77,3 @@ export default Controller.extend(AuthenticatedController, {
         
   @action
   timezoneChanged(val) {
-    this.set('model.timezone', val);
-  }
-        
->>>>>>> upstream/master
-});
